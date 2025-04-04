69d28a07b30d093722f2a06de33d7a1207519b0d
Musta-Hattu <mustahattu@tutamail.com>
2025-04-04 14:37:19 +0200

    cleaned up the code

--- src/main.rs
+++ src/main.rs
@@ -33,10 +33,9 @@ fn into_cell(pos:f32) -> f32 {
     unsafe { (pos - ((RES - HEIGHT * CELL_SIZE) / 2) as f32) / CELL_SIZE as f32 }
 }
 
-fn parse_args() -> (bool,bool) {
+fn parse_args() -> bool {
     let args:Vec<String> = env::args().skip(1).collect();
     let mut full = false;
-    let mut multi_hit = false;
     let mut i = 0;
     while i < args.len() {
         match args[i].as_str() {
@@ -58,10 +57,6 @@ fn parse_args() -> (bool,bool) {
                 println!("depth = {}",args[i + 1]);
                 i += 2;
             }}
-            "-a" => { 
-                multi_hit = true;
-                i += 1;
-            }
             _ => panic!("not a valid flag \"{}\"",args[i]),
         }
     }
@@ -70,7 +65,7 @@ fn parse_args() -> (bool,bool) {
         TREE_RES =  RES * 9/10 ;
         CELL_SIZE =  TREE_RES/HEIGHT ;
     }
-    return (full,multi_hit);
+    return full;
 }
 
 
@@ -81,7 +76,7 @@ fn clear_screen() {
 }
 
 fn main() {
-    let (full,multi_hit) = parse_args();
+    let full = parse_args();
     let mut quadtree;
     if full {
         quadtree = unsafe { Quadtree::new_full(HEIGHT,ivec2!(0,0)) };
@@ -94,17 +89,6 @@ fn main() {
     let mut target_y = unsafe { RES as f32/2. - 1e-5 };
 
     'draw_loop: while app.window.is_open() {
-        print!("len {} ",quadtree.nodes.len());
-        for (i,node) in quadtree.nodes.iter().enumerate() {
-            print!("{} ",if node.is_orphan { 1 } else { 0 });
-            if i % 4 == 0 {
-                print!("|");
-            }
-            if i % unsafe {HEIGHT} as usize == 0 {
-                println!();
-            }
-        }
-        println!();
         let fb = &mut app.framebuffer;
 
         fb.clear(BG_COL);
@@ -121,42 +105,28 @@ fn main() {
             let ray_dir = Vec2{ x: cell_target_x - cell_mouse_x, 
                                 y: cell_target_y - cell_mouse_y }.norm();
 
-            //let start = Instant::now();
-            //for _ in 0..1000 {
-                //dda_quad(ray_origin,ray_dir,1000.,&quadtree);
-            //}
-            //println!("dda   {:?}",start.elapsed());
-//
-            //let start = Instant::now();
-            //for _ in 0..1000 {
-                //raycast2(ray_origin,ray_dir,&quadtree);
-            //}
-            //println!("param {:?}",start.elapsed());
+            let start = Instant::now();
+            for _ in 0..1000 {
+                dda_quad(ray_origin,ray_dir,1000.,&quadtree);
+            }
+            println!("dda   {:?}",start.elapsed());
+
+            let start = Instant::now();
+            for _ in 0..1000 {
+                raycast2(ray_origin,ray_dir,&quadtree);
+            }
+            println!("param {:?}",start.elapsed());
 
 
-            if multi_hit {
-                //let collitions = raycast(ray_origin,ray_dir,&quadtree);
-
-                //for (i,node) in collitions.iter().enumerate() {
-                    //let red = Color { col: 0x00FF0000 };
-                    //let blue = Color { col: 0x000000FF };
-                    //unsafe { 
-                        //let col = blend_color(blue,red, i as f32 / collitions.len() as f32).col;
-                        //fb.square(from_cell(node.position.x) ,from_cell(node.position.y), node.size * CELL_SIZE , col);
-                    //}
-                //}
-
-            } else {
-                let collition = raycast2(ray_origin,ray_dir,&quadtree);
-                
-                if let Some((first_node,t)) = collition {
-                    unsafe {
-                        fb.empty_square(from_cell(first_node.position.x) ,   from_cell(first_node.position.y) , first_node.size * CELL_SIZE , GREEN);
-                        fb.empty_square(from_cell(first_node.position.x) + 1,from_cell(first_node.position.y) + 1, first_node.size * CELL_SIZE -2, GREEN);
-                    }
-                    let hit_pos = ray_origin + ray_dir * t;
-                    fb.circle(from_cell_f32(hit_pos.x),from_cell_f32(hit_pos.y),4,PINK);
+            let collition = raycast2(ray_origin,ray_dir,&quadtree);
+            
+            if let Some((first_node,t)) = collition {
+                unsafe {
+                    fb.empty_square(from_cell(first_node.position.x) ,   from_cell(first_node.position.y) , first_node.size * CELL_SIZE , GREEN);
+                    fb.empty_square(from_cell(first_node.position.x) + 1,from_cell(first_node.position.y) + 1, first_node.size * CELL_SIZE -2, GREEN);
                 }
+                let hit_pos = ray_origin + ray_dir * t;
+                fb.circle(from_cell_f32(hit_pos.x),from_cell_f32(hit_pos.y),4,PINK);
             }
 
             let mouse_x = mouse_x.round() as i32;
@@ -185,24 +155,6 @@ fn main() {
                 quadtree.remove_block(ivec2!(grid_x ,grid_y ));
             }
         }
-        //if app.window.get_mouse_down(MouseButton::Middle) {
-            //if let Some((mouse_x,mouse_y)) = app.window.get_mouse_pos(MouseMode::Discard) {
-                //let grid_x = into_cell(mouse_x).floor() as i32;
-                //let grid_y = into_cell(mouse_y).floor() as i32;
-                ////grid[grid_x][grid_y] = false;
-                //let mut out = String::new();
-                //if quadtree.is_solid_at(ivec2!(grid_x ,grid_y )) {
-                    //out += "is solid";
-                //} else {
-                    //out += "not solid";
-                //}
-                //println!("{} size {} idx {}",
-                            //out,
-                            //quadtree.size_at(ivec2!(grid_x as i32,grid_y as i32)),
-                            //quadtree.index_at(ivec2!(grid_x as i32,grid_y as i32)),
-                            //);
-            //}
-        //}
 
         for key in app.window.get_keys() {
             use minifb::Key;
@@ -215,6 +167,18 @@ fn main() {
                 _ => (),
             }
         }
+        print!("len {} ",quadtree.nodes.len());
+        for (i,node) in quadtree.nodes.iter().enumerate() {
+            print!("{} ",if node.is_orphan { 1 } else { 0 });
+            if i % 4 == 0 {
+                print!("|");
+            }
+            if i % unsafe {HEIGHT} as usize == 0 {
+                println!();
+            }
+        }
+        println!();
+
         app.display();
         clear_screen();
     }
@@ -225,28 +189,23 @@ pub fn dda_quad(start: Vec2, dir: Vec2, max_distance: f32,chunk_data:&Quadtree) 
     let mut voxel = IVec2::new(
                             start.x.floor() as i32, 
                             start.y.floor() as i32, 
-                            //start.z.floor() as i32 
                         );
 
     let step_dir = IVec2::new(
                             dir.x.signum() as i32 ,
                             dir.y.signum() as i32 ,
-                            //dir.z.signum() as i32
                         );
 
     let t_delta = Vec2::new( 
                             1. / dir.x.abs(), 
                             1. / dir.y.abs(), 
-                            //1. / dir.z.abs() 
                         );
 
     fn frac0(x: f32) -> f32 {
-        //x - (x / size).floor() * size
         x - x.floor()
     }
 
     fn frac1(x: f32) -> f32 {
-        //size - frac0(x,size)
         1. - frac0(x)
     }
 
@@ -262,18 +221,12 @@ pub fn dda_quad(start: Vec2, dir: Vec2, max_distance: f32,chunk_data:&Quadtree) 
         t_delta.y * frac0(start.y)
     };
 
-    //let end =  start + dir * max_distance;
-    //fb.circle((end.x * CELL_SIZE as f32).round() as i32,(end.y * CELL_SIZE as f32).round() as i32,3,YELLOW);
-
     let mut traveled_distance = 0.0;
     while traveled_distance < max_distance {
 
         if chunk_data.is_solid_at(voxel) {
             return Some((voxel,start + dir * traveled_distance));
         }
-
-        //let curr = start + dir * traveled_distance;
-        //fb.circle((curr.x * CELL_SIZE as f32).round() as i32 ,(curr.y * CELL_SIZE as f32).round() as i32 ,2,PINK);
 
         if t_max_x < t_max_y {
             voxel.x += step_dir.x;
